a321bb5c946f82ebd4c6b714fedd944644c357fc
phoon0618 <48398804+phoon0618@users.noreply.github.com>
2019-03-11 11:51:54 +0800

    Add files via upload

--- pages/DashboardPage.php
+++ pages/DashboardPage.php
@@ -21,17 +21,26 @@ else
 <html>
 <head>
 	<title>Dashboard Page</title>
-	
+	<link rel='stylesheet' href='fullcalendar/fullcalendar.css'/>
+	<script src='fullcalendar/lib/jquery.min.js'></script>
+	<script src='fullcalendar/lib/moment.min.js'></script>
+	<script src='fullcalendar/fullcalendar.js'></script>
+	<script type='text/javascript' src='fullcalendar/gcal.js'></script>
 	<script >
 		$(function() {
-		  
+		  $('#calendar').fullCalendar({
+			header:{
+				  left:"prev,next,today",
+				  center:"title",
+				  right:"month,listDay"
+			  },
 			eventSources: [
 			{
 				googleCalendarApiKey: "AIzaSyCH4g3WsOSKOy5gUFQu-A71MrHlvtgxxgQ",
 				googleCalendarId: "<?php echo $userData['email']?>"
 			}]
 				  
-		  
+		  })
 		});
 	</script>
 </head>
