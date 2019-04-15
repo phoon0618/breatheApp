60226c1062bd587d00bd736a39391e92ada3bd3b
ChloeYeeLi <yeeli97@gmail.com>
2019-04-15 09:09:56 +0800

    reminder

--- DashboardPage.php
+++ DashboardPage.php
@@ -1,25 +1,63 @@
 <?php 
+
 require_once "config.php";
 
-if(isset($_SESSION['access_token']))
-    $gClient->setAccessToken($_SESSION['access_token']);
+if(isset($_SESSION['access_token'])){
+	$authUrl = $gClient->createAuthUrl();
+	$gClient->setAccessToken($_SESSION['access_token']);}
 else if(isset($_GET['code'])){
-    $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
-    $_SESSION['access_token']=$token;
+	$token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
+	$_SESSION['access_token']=$token;
 }
-else{
-    header('Location:LoginPage.php');
+else 
+{
+	header('Location:LoginPage.php');
 }
 
-// Get the API client and construct the service object.
-$oAuth = new Google_Service_Oauth2($gClient);
-$service = new Google_Service_Calendar($gClient);
-$userData =	$oAuth->userinfo_v2_me->get();
+	$oAuth = new Google_Service_Oauth2($gClient);
+	$userData =	$oAuth->userinfo_v2_me->get();
+	$service = new Google_Service_Calendar($gClient);
+	
+   // get event from the google calendar
+  $calendarId = 'primary';
+  $eventDay = array(
+    'orderBy' => 'startTime',
+    'singleEvents' => TRUE,
+	'timeMax' => date('c', mktime(0,0,0, date('m'), date('d')+1, date('y'))),
+    'timeMin' => date('c'),
+  );
+  
+  $results = $service->events->listEvents($calendarId, $eventDay);
+  $durationBusyHours=0;
+  $event_details=array();	
 
-$calendarId = 'primary';
+  if (count($results->getItems()) != 0) {
+    foreach ($results->getItems() as $event) {
+	  $title = $event ->getsummary();
+      $start = $event->start->dateTime;
+	  $end = $event->end->dateTime;
+	 
+      if (empty($start)) {
+        $start = $event->start->date;
+      }
+	  if(empty($end))
+	  {
+		 $end = $event->end->date;  
+	  }
 
-//get this week events
-$optParams = array(
+      $startEvent = strtotime($start);
+      $endEvent = strtotime($end);
+	  $durationBusyHours += ($endEvent - $startEvent)/3600; //in hours
+	  
+	  $start = new DateTime($start);
+	  $end = new DateTime($end);
+	  array_push($event_details,array("title"=>$title,"startTime"=>$start->format('H:i:s'),"endTime"=>$end->format('H:i:s'))); 
+    } 
+  }
+   
+   
+   	//get this week events
+$eventWeek = array(
     'orderBy' => 'startTime',
     'singleEvents' => true,
     'timeMin' => getMinDate(),
@@ -31,7 +69,7 @@ $sleepHours = 8;
 $stressLevelsBusy= array();
 $stressLevelsFree= array();
 
-$results = $service->events->listEvents($calendarId, $optParams);
+$results = $service->events->listEvents($calendarId, $eventWeek);
 $events = $results->getItems();
 
 //generate datasets for charts
@@ -93,9 +131,7 @@ function getDayOfWeekNumber($date){
             return "5";
         
         case 'Saturday':
-            return "6";
-        
-        
+            return "6";  
     }
 }
 
@@ -114,78 +150,139 @@ function getMaxDate(){
         strtotime("next saturday");
     return date('c', $maxDate);
 }
+ 
 
 ?>
-<!DOCTYPE HTML>
-<html>
 
+<!Doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
+	<title>Dashboard Page</title>
+	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard Page</title>
-    <link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!-- Chart Scripts -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js'>
-    </script>
+    </script>	
+	
+	<!-- Calendar Scripts -->
+	<link rel='stylesheet' href='../fullcalendar/fullcalendar.css'/>
+	<script src='../fullcalendar/lib/jquery.min.js'></script>
+	<script src='../fullcalendar/lib/moment.min.js'></script>
+	<script src='../fullcalendar/fullcalendar.js'></script>
+	<script src='../fullcalendar/gcal.js'></script>	
+	<script src='../node_modules/push.js/bin/push.min.js'></script>
+	
+	
+	<script>
+		$(function() {
+		  $('#calendar').fullCalendar({
+			plugins: [ 'googleCalendar' ],
+			header:{
+				  left:"prev,next today myCustomButton",
+				  center:"title",
+				  right:"month,listMonth,listWeek,listDay"
+			  },
+			  buttonText: {
+				month: 'Month',
+				listMonth: 'List Month',
+				listYear: 'List Year',
+				listWeek: 'List Week',
+				listDay: 'List Day'
+			},
+			  selectable:true,
+			  selectHelper:true,
+			  editable:true,
+			  droppable: true, 
 
-    <style type="text/css">
-    #chart-container {
-        width: 100%;
-        height: auto;
-    }
-    </style>
+			eventSources: [
+			{
+				googleCalendarApiKey: "AIzaSyCH4g3WsOSKOy5gUFQu-A71MrHlvtgxxgQ",
+				googleCalendarId: "<?php echo $userData['email']?>"			
+			}],
+			
+			 eventClick: function(event){
+     
+			},
+			
+			 dayClick: function() {
+			 
+			 },
+		
+			
+		  })
+		});
+		
+		function reminder(){
+			Push.create("Take a break",{
+				body: "Your busy time is more 8 hours" ,
+				timeout:6000,
+			});
+		}
+		
+	// reminder to drink water every 1 hours
+    var i = 0;
+    var timer = setInterval(function() {
+        Push.create("Hydrate reminder", {
+		body: "It's time to drink some water!",
+		icon: '/icon.png',
+		timeout: 4000,
+		   
+		onClick: function () {
+			window.focus();
+			this.close();
+		}
+	});
+    }, 3600000);
+	
+	
+	$(document).ready(function() {
+		setInterval(timestamp, 1000);
+		
+	});
 
-    <!-- Calendar Scripts -->
-    <script src='fullcalendar/lib/jquery.min.js'></script>
-    <script src='fullcalendar/lib/moment.min.js'></script>
-    <script src='fullcalendar/fullcalendar.js'></script>
-    <script type='text/javascript' src='fullcalendar/gcal.js'></script>
-
-    <script>
-    $(function() {
-        $('#calendar').fullCalendar({
-            header: {
-                left: "prev,next,today",
-                center: "title",
-                right: "month, listWeek"
-            },
-            eventSources: [{
-                googleCalendarApiKey: "AIzaSyCH4g3WsOSKOy5gUFQu-A71MrHlvtgxxgQ",
-                googleCalendarId: "<?php echo $userData['email']?>"
-            }]
-
-        })
-    });
-    </script>
-
+	function timestamp() {
+		$.ajax({
+			url: 'http://localhost/breatheApp/pages/timestamp.php',
+			success: function(data) {
+				$('#timestamp').html(data);	
+				
+				<?php foreach($event_details as $notification){?>
+				if(data == '<?=$notification['startTime']?>'){
+					Push.create('<?=$notification['title']?>',{
+						body: '<?=$notification['startTime']?>'+" ~ "+'<?=$notification['startTime']?>' ,
+						timeout:6000,
+					});
+				}
+				<?php } ?>
+				
+			},
+		});
+	}	
+	
+	</script>
+	
 </head>
 
 <body>
-    <ul class="nav justify-content-end">
-        <!-- <li class="nav-item">
-            <a id="chartButton" class="nav-link" href="HelpPage.php">How to Use</a>
-        </li> -->
-        <li class="nav-item">
-            <a id="logoutButton" class="nav-link btn btn-danger" href="LogoutPage.php">Sign Out</a>
-        </li>
-    </ul>
+<!-- Navigation bar -->	
+<ul>
+	<li><a href="ReminderPage.php">Set Reminder</a></li>
+	<li><a href="ShareCalendar.php">Share Calendar</a></li>
+	<li><a href="LogoutPage.php">Sign Out</a></li>
+</ul>
 
-    <div id="content"></div>
-    <div class="container">
-        <div id="calendar"></div>
-    </div>
-
-    <div class="container">
-        <div id="chart-container">
-            <canvas id="graphCanvas"></canvas>
-        </div>
-    </div>
-
-    <!-- rush level scripts -->
+<!-- Content -->	
+<div id="timestamp"></div>
+<div id="calendar"></div>
+<canvas id="graphCanvas"></canvas>
+	
+<?php if($durationBusyHours>8){
+		echo "<script> reminder();</script>";
+	}?>
+	
+	<!-- rush level scripts -->
     <script>
     $(document).ready(function() {
         showGraph();
@@ -246,6 +343,7 @@ function getMaxDate(){
         }
     }
     </script>
-</body>
 
+</body>
 </html>
+
